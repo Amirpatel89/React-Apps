5e7af1859b68fc58ccdef04c494d8bed225855ff
Amir Patel <31699548+Amirpatel89@users.noreply.github.com>
2017-11-30 14:48:27 -0500

    Add files via upload

---
+++ Inventory:Temperature/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import TemperatureApp from './tempApp/TemperatureApp';
+
+ReactDOM.render(
+	<App />, 
+	// <TemperatureApp />,	
+	document.getElementById('root')
+);
